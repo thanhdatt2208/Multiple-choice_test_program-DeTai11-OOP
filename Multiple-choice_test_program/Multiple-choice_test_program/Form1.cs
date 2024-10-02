02d06ec12e78bd65b8ca073e9e5e91bbd5eb1be4
Nelson Duong <duongkhainghiemm@gmail.com>
2024-10-02 10:00:48 +0700

    incomplete

---
+++ Multiple-choice_test_program/Multiple-choice_test_program/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multiple_choice_test_program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timerDate.Tick += new EventHandler(timerDate_Tick);
+            timerDate.Start();
+        }
+
+        private void timerDate_Tick(object sender, EventArgs e)
+        {
+            labelDate.Text = DateTime.Now.ToString("dd/MM/yyyy");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
